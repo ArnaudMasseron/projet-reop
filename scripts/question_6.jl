3ec832af4a7a553b66cc3f7d7c42d6af663d5cc9
Masseron Arnaud <masseron.arnaud@gmail.com>
2023-12-29 15:19:49 +0100

    Correction termes quadratiques fonction objectif

--- scripts/question_6.jl
+++ scripts/question_6.jl
@@ -13,6 +13,9 @@ Le fichier tiny_sol.json ne semble pas etre la solution optimale pour l'instance
 un peu la solution codee dans ce fichier on peut facilement faire baisser le cout.
 
 Il faut encore coder la fonction qui permet d'ecrire la solution dans un fichier JSON.
+
+Le programme est tres lent avec l'instance small: je l'ai fait tourner pendant une heure et il ca ne m'avait
+pas fourni de solution.
 """
 
 
@@ -20,7 +23,7 @@ using JSON, JuMP, Gurobi
 import LinearAlgebra: norm
 
 ### Chargement des donnees
-file_path = joinpath(@__DIR__, "instances/KIRO-tiny_20.json")
+file_path = joinpath(@__DIR__, "instances/KIRO-tiny.json")
 data = JSON.parsefile(file_path)
 
 Vˢ = data["substation_locations"]
@@ -109,17 +112,16 @@ function linearize_min(α, β, M)
     return -linearize_positive_part(β - α, 2*M) + β
 end
 
-
 # Construction de Cⁿ
 max_rating_S = maximum([S[s]["rating"] for s ∈ indices_S])
 max_rating_Q⁰ = maximum([Q⁰[q]["rating"] for q ∈ indices_Q⁰])
-max_rating_S_Q⁰ = maximum([max_rating_S, max_rating_Q⁰])
-Cⁿ = [sum(linearize_positive_part(Ω[ω]["power_generation"] * sum(z[v, t] for t ∈ indices_Vᵗ) - linearize_min(sum(S[s]["rating"] * x[v, s] for s ∈ indices_S), sum(Q⁰[q]["rating"] * y⁰[v, q] for q ∈ indices_Q⁰), max_rating_S_Q⁰), maximum([Ω[ω]["power_generation"]*length(Vᵗ), minimum([max_rating_Q⁰, max_rating_S])])) for v ∈ indices_Vˢ) for ω ∈ indices_Ω]
+max_rating_S_Q⁰ = max(max_rating_S, max_rating_Q⁰)
+Cⁿ = [sum(linearize_positive_part(Ω[ω]["power_generation"] * sum(z[v, t] for t ∈ indices_Vᵗ) - linearize_min(sum(S[s]["rating"] * x[v, s] for s ∈ indices_S), sum(Q⁰[q]["rating"] * y⁰[v, q] for q ∈ indices_Q⁰), max_rating_S_Q⁰), max(Ω[ω]["power_generation"]*length(Vᵗ), min(max_rating_Q⁰, max_rating_S))) for v ∈ indices_Vˢ) for ω ∈ indices_Ω]
 
 # Construction de Cᶠ
 max_rating_Qˢ = maximum([Qˢ[q]["rating"] for q ∈ indices_Qˢ])
-Cᶠ = [linearize_positive_part(Ω[ω]["power_generation"] * sum(z[v, t] for t ∈ indices_Vᵗ) - sum(Qˢ[q]["rating"] * yˢ[ẽ, q] for ẽ ∈ indices_Eˢ, q ∈ indices_Qˢ if v ∈ Eˢ[ẽ]), Ω[ω]["power_generation"]*length(Vᵗ)+max_rating_Qˢ) +  
-      sum(linearize_positive_part(Ω[ω]["power_generation"] * sum(z[(Eˢ[ẽ][1]!=v ? Eˢ[ẽ][1] : Eˢ[ẽ][2]), t] for t ∈ indices_Vᵗ) + linearize_min(sum(Qˢ[q]["rating"]*yˢ[ẽ, q] for q ∈ indices_Qˢ), Ω[ω]["power_generation"] * sum(z[v, t] for t ∈ indices_Vᵗ), max_rating_Qˢ + Ω[ω]["power_generation"] * length(Vᵗ)) - linearize_min(sum(S[s]["rating"] * x[(Eˢ[ẽ][1]!=v ? Eˢ[ẽ][1] : Eˢ[ẽ][2]), s] for s ∈ indices_S), sum(Q⁰[q]["rating"] * y⁰[(Eˢ[ẽ][1]!=v ? Eˢ[ẽ][1] : Eˢ[ẽ][2]), q] for q ∈ indices_Q⁰), max_rating_S_Q⁰), maximum([Ω[ω]["power_generation"]*length(Vᵗ) + minimum([max_rating_Qˢ, Ω[ω]["power_generation"] * length(Vᵗ)]), minimum([max_rating_Q⁰, max_rating_S])])) for ẽ ∈ indices_Eˢ if v ∈ Eˢ[ẽ])
+Cᶠ = [linearize_positive_part(Ω[ω]["power_generation"] * sum(z[v, t] for t ∈ indices_Vᵗ) - sum(Qˢ[q]["rating"] * yˢ[ẽ, q] for ẽ ∈ indices_Eˢ, q ∈ indices_Qˢ if v ∈ Eˢ[ẽ]), max(Ω[ω]["power_generation"]*length(Vᵗ), max_rating_Qˢ)) +  
+      sum(linearize_positive_part(Ω[ω]["power_generation"] * sum(z[(Eˢ[ẽ][1]!=v ? Eˢ[ẽ][1] : Eˢ[ẽ][2]), t] for t ∈ indices_Vᵗ) + linearize_min(sum(Qˢ[q]["rating"]*yˢ[ẽ, q] for q ∈ indices_Qˢ), Ω[ω]["power_generation"] * sum(z[v, t] for t ∈ indices_Vᵗ), max(max_rating_Qˢ, Ω[ω]["power_generation"] * length(Vᵗ))) - linearize_min(sum(S[s]["rating"] * x[(Eˢ[ẽ][1]!=v ? Eˢ[ẽ][1] : Eˢ[ẽ][2]), s] for s ∈ indices_S), sum(Q⁰[q]["rating"] * y⁰[(Eˢ[ẽ][1]!=v ? Eˢ[ẽ][1] : Eˢ[ẽ][2]), q] for q ∈ indices_Q⁰), max_rating_S_Q⁰), max(Ω[ω]["power_generation"]*length(Vᵗ) + min(max_rating_Qˢ, Ω[ω]["power_generation"] * length(Vᵗ)), min(max_rating_Q⁰, max_rating_S))) for ẽ ∈ indices_Eˢ if v ∈ Eˢ[ẽ])
       for v ∈ indices_Vˢ, ω ∈ indices_Ω]
 
 # Construction de Cᶜ(Cⁿ) et Cᶜ(Cᶠ)
@@ -127,19 +129,22 @@ c₀ = data["general_parameters"]["curtailing_cost"]
 cₚ = data["general_parameters"]["curtailing_penalty"]
 Cₘₐₓ = data["general_parameters"]["maximum_curtailing"]
 
-Cᶜ_Cⁿ = [c₀*Cⁿ[ω] + cₚ*linearize_positive_part(Cⁿ[ω]-Cₘₐₓ, maximum([Ω[ω]["power_generation"]*length(Vᵗ), Cₘₐₓ])) for ω ∈ indices_Ω]
-Cᶜ_Cᶠ = [c₀*Cᶠ[v, ω] + cₚ*linearize_positive_part(Cᶠ[v, ω]-Cₘₐₓ, maximum([(Ω[ω]["power_generation"]*length(Vᵗ) + minimum([max_rating_Qˢ, Ω[ω]["power_generation"]*length(Vᵗ)])) * length(Vˢ), Cₘₐₓ])) for v ∈ indices_Vˢ, ω ∈ indices_Ω]
+borne_Cⁿ = [Ω[ω]["power_generation"]*length(Vᵗ) for ω ∈ indices_Ω]
+borne_Cᶠ = [(Ω[ω]["power_generation"]*length(Vᵗ) + min(max_rating_Qˢ, Ω[ω]["power_generation"]*length(Vᵗ))) * length(Vˢ) for v ∈ indices_Vˢ, ω ∈ indices_Ω]
 
-# Construction de pᶠ
-pᶠ = [sum(S[s]["probability_of_failure"]*x[v, s] for s ∈ indices_S) + sum(Q⁰[q]["probability_of_failure"]*y⁰[v, q] for q ∈ indices_Q⁰) for v ∈ indices_Vˢ]
+Cᶜ_Cⁿ = [c₀*Cⁿ[ω] + cₚ*linearize_positive_part(Cⁿ[ω]-Cₘₐₓ, max(borne_Cⁿ[ω], Cₘₐₓ)) for ω ∈ indices_Ω]
+Cᶜ_Cᶠ = [c₀*Cᶠ[v, ω] + cₚ*linearize_positive_part(Cᶠ[v, ω]-Cₘₐₓ, max(borne_Cᶠ[v, ω], Cₘₐₓ)) for v ∈ indices_Vˢ, ω ∈ indices_Ω]
 
 # Construction de la fonction objectif C
 coords_v₀ = [data["general_parameters"]["main_land_station"]["x"], data["general_parameters"]["main_land_station"]["y"]]
+borne_Cᶜ_Cⁿ = [c₀ * borne_Cⁿ[ω] + cₚ * max(0, borne_Cⁿ[ω] - Cₘₐₓ) for ω ∈ indices_Ω]
+borne_Cᶜ_Cᶠ = [c₀ * borne_Cᶠ[v, ω] + cₚ * max(0, borne_Cᶠ[v, ω] - Cₘₐₓ) for v ∈ indices_Vˢ, ω ∈ indices_Ω]
+
 c = sum(S[s]["cost"]*x[v, s] for v ∈ indices_Vˢ, s ∈ indices_S) +
     sum((Q⁰[q]["fixed_cost"] + norm([Vˢ[v]["x"], Vˢ[v]["y"]] - coords_v₀) * Q⁰[q]["variable_cost"]) * y⁰[v, q] for v ∈ indices_Vˢ, q ∈ indices_Q⁰) + 
     sum((Qˢ[q]["fixed_cost"] + norm([Vˢ[Eˢ[e][1]]["x"], Vˢ[Eˢ[e][1]]["y"]] - [Vˢ[Eˢ[e][2]]["x"], Vˢ[Eˢ[e][2]]["y"]]) * Qˢ[q]["variable_cost"]) * yˢ[e, q] for e ∈ indices_Eˢ, q ∈ indices_Qˢ) + 
     sum((data["general_parameters"]["fixed_cost_cable"] + norm([Vˢ[v]["x"], Vˢ[v]["y"]] - [Vᵗ[t]["x"], Vᵗ[t]["y"]]) * data["general_parameters"]["variable_cost_cable"]) * z[v, t] for v ∈ indices_Vˢ, t ∈ indices_Vᵗ) + 
-    sum(Ω[ω]["probability"] * (sum(pᶠ[v] * Cᶜ_Cᶠ[v, ω] for v ∈ indices_Vˢ) + (1 - sum(pᶠ)) * Cᶜ_Cⁿ[ω]) for ω ∈ indices_Ω)
+    sum(Ω[ω]["probability"] * (sum(sum(S[s]["probability_of_failure"] * linearize_product(x[v, s], Cᶜ_Cᶠ[v, ω], borne_Cᶜ_Cᶠ[v, ω]) for s ∈ indices_S) + sum(Q⁰[q]["probability_of_failure"]* linearize_product(y⁰[v, q], Cᶜ_Cᶠ[v, ω], borne_Cᶜ_Cᶠ[v, ω]) for q ∈ indices_Q⁰) for v ∈ indices_Vˢ) + Cᶜ_Cⁿ[ω] - sum(sum(S[s]["probability_of_failure"] * linearize_product(x[v, s], Cᶜ_Cⁿ[ω], borne_Cᶜ_Cⁿ[ω]) for s ∈ indices_S) + sum(Q⁰[q]["probability_of_failure"]* linearize_product(y⁰[v, q], Cᶜ_Cⁿ[ω], borne_Cᶜ_Cⁿ[ω]) for q ∈ indices_Q⁰) for v ∈ indices_Vˢ)) for ω ∈ indices_Ω)
 
 # Declaration de la fonction objectif
 @objective(model, Min, c)
